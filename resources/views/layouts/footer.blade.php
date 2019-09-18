3b70b23b7d814b5c333c060111320ceaca41dc4e
asrani <asrani.27@gmail.com>
2019-09-18 13:01:03 +0800

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,9 @@
 
-  <footer class="main-footer">
+  <footer class="main-foote">
     <div class="container">
       <div class="pull-right hidden-xs">
         <b>Version</b> 1.1.1
       </div>
-      <strong>Copyright &copy; 2019 CSS Kota Banjarmasin 2019.</strong>
     </div>
     <!-- /.container -->
   </footer>
